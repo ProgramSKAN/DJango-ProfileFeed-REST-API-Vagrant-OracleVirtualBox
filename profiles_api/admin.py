cf17f84b0c4e83bd35d8dbc49c110e0c7bcdccf5
ProgramSKAN <kandagatlas@gmail.com>
2020-05-16 20:34:40 +0530

    Added Feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -4,3 +4,4 @@ from profiles_api import models
 # Register your models here.
 
 admin.site.register(models.UserProfile)  # register our user profile model with the admin site
+admin.site.register(models.ProfileFeedItem)
